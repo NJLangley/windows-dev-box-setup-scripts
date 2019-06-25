6a03ef3a18b722cb23df43280c62667792afd0cf
Niall <niall.langley@gmail.com>
2019-06-25 11:27:55 +0100

    try and fix bugs

--- scripts/CommonBoxstarterConfig.ps1
+++ scripts/CommonBoxstarterConfig.ps1
@@ -1,2 +1,4 @@
-$ChocoCacheLocation = "$env:userprofile\AppData\Local\Temp\chocolatey\";
-New-Item -Path $ChocoCacheLocation -ItemType directory -Force | Out-Null;
+#$ChocoCacheLocation = "$env:userprofile\AppData\Local\Temp\chocolatey\";
+#New-Item -Path $ChocoCacheLocation -ItemType directory -Force | Out-Null;
+
+New-Item -Path "c:\Temp\chocolatey\" -ItemType directory -Force | Out-Null;
